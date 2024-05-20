16b53ab5ba2604d22b032d7db3d2bde9ea934ff5
@KEfremov <kirill.efremov@re-coders.pro>
2024-05-20 21:30:23 +0300

    Initial Commit

---
+++ dbke/dbkeApp.swift
@@ -0,0 +1,17 @@
+//
+//  dbkeApp.swift
+//  dbke
+//
+//  Created by Kirill Efremov on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct dbkeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
